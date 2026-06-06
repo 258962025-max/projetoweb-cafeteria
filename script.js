dafb3373f54983bfb44ddc259497c80472256af2
Guilherme Henrique Cavalher Silva <guilhermecavalher@gmail.com>
2026-06-06 20:46:07 -0300

    Atualiza slogan

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+// Início do Banco de Dados 
+
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [
+    { id: 1, nome: "Café Expresso", preco: 5.00 },
+    { id: 2, nome: "Cappuccino", preco: 8.00 }
+];
+
+function salvarBanco() {
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+}
+
+function mostrarProdutos() {
+    const container = document.getElementById("lista-produtos");
+
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    produtos.forEach(p => {
+        container.innerHTML += `<p>${p.nome} - R$ ${p.preco.toFixed(2)}</p>`;
+    });
+}
+
+// Fim do Banco de Dados
+
+function adicionarProduto(nome, preco) {
+    produtos.push({
+        id: Date.now(),
+        nome,
+        preco
+    });
+
+    salvarBanco();
+    mostrarProdutos();
+}
+
+function add() {
+    const nome = document.getElementById("nome").value;
+    const preco = parseFloat(document.getElementById("preco").value);
+
+    adicionarProduto(nome, preco);
+}
+
+mostrarProdutos();
+
 console.log("Site carregado com sucesso!");
